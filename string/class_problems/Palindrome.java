99bee2b95550b4e414a63e92d4b7a37f16815eeb
Kriti Srivastav <kritisri1175@gmail.com>
2026-09-05 08:49:37 +0530

    Add Problem 3

---
+++ string/class_problems/Palindrome.java
@@ -0,0 +1,58 @@
+
+import java.util.Scanner;
+
+class palindrome {
+
+    public static boolean isPalindromeIterative(String text) {
+        int left = 0;
+        int right = text.length() - 1;
+
+        while (left < right) {
+            if (text.charAt(left) != text.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+
+    public static boolean isPalindromeRecursive(String text) {
+        if (text.length() <= 1) {
+            return true;
+        }
+
+        if (text.charAt(0) != text.charAt(text.length() - 1)) {
+            return false;
+        }
+
+        return isPalindromeRecursive(text.substring(1, text.length() - 1));
+    }
+
+    public static boolean isPalindromeArrayReversal(String text) {
+        char[] original = text.toCharArray();
+        char[] reversed = new char[original.length];
+
+        for (int i = 0; i < original.length; i++) {
+            reversed[i] = original[original.length - 1 - i];
+        }
+
+        return new String(original).equals(new String(reversed));
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a word or phrase: ");
+        String text = sc.nextLine();
+
+        boolean iterative = isPalindromeIterative(text);
+        boolean recursive = isPalindromeRecursive(text);
+        boolean arrayReversal = isPalindromeArrayReversal(text);
+
+        System.out.println("Iterative: " + iterative);
+        System.out.println("Recursive: " + recursive);
+        System.out.println("Array Reversal: " + arrayReversal);
+    }
+}
